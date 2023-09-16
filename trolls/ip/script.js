b18e68a46ec986bd46cfe7a5b640bf1ccc627194
Bypass6 <139152259+Bypass6@users.noreply.github.com>
2023-09-16 15:49:42 -0400

    Update script.js

--- trolls/ip/script.js
+++ trolls/ip/script.js
@@ -6,27 +6,21 @@ window.onload = async () => {
     throw message;
   };
 
+  const addMessage = (message) => {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("message");
+    messageElement.textContent = message;
+    data.appendChild(messageElement);
+  };
+
   try {
     let my_ip = await (await fetch("https://wtfismyip.com/json").catch()).json().catch();
 
-    const push = (title, content, add) => {
-      const el = document.createElement("span");
-      el.textContent = `${title}: ${content}${add || ""}`;
-      data.appendChild(el);
-    };
-
     if (my_ip) {
-      push("IP Address", my_ip.YourFuckingIPAddress);
-      push("Hostname", my_ip.YourFuckingHostname);
-      push("OS", navigator.platform);
+      addMessage(`Your IP Address is: ${my_ip.YourFuckingIPAddress}`);
     } else {
-      push("IP Address", "::ffff:172.70.126.134");
-      push("Hostname", "Unknown");
-      push("OS", navigator.platform);
+      addMessage(`IP Address: ::ffff:172.70.126.134`);
     }
-
-    let date = new Date();
-    push("Current Time", `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`);
 
   } catch (e) {
     error(`${e.message}`);
